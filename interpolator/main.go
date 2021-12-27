ba460ecbbb1f0c2b3bfcb45c147c191c1f32c6a9
Onur YURDUPAK <onuryurdupak@gmail.com>
2021-12-28 00:11:31 +0300

    Add exit codes

--- interpolator/main.go
+++ interpolator/main.go
@@ -10,22 +10,28 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	errInput    = 1
+	errInternal = 2
+	errUnkown   = 3
+)
+
 func main() {
 	args := os.Args[1:]
 	if len(args) < 2 {
 		fmt.Println("Define a file path and at least one key=value pair.")
-		os.Exit(1)
+		os.Exit(errInput)
 	}
 
 	_, err := os.Stat(args[0])
 	if err != nil {
 		fmt.Printf("Error checking file at: '%s' error: '%s'.\n", args[0], err.Error())
-		os.Exit(2)
+		os.Exit(errUnkown)
 	}
 
 	if os.IsNotExist(err) {
 		fmt.Printf("File does not exist: '%s'.\n", args[0])
-		os.Exit(3)
+		os.Exit(errInput)
 	}
 
 	fileContent, err := ioutil.ReadFile(args[0])
@@ -33,7 +39,7 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("Error reading file at: '%s' error: '%s'.\n", args[0], err.Error())
-		os.Exit(4)
+		os.Exit(errUnkown)
 	}
 
 	replaces := map[string]string{}
@@ -43,18 +49,18 @@ func main() {
 
 		if len(split) != 2 {
 			fmt.Printf("Invalid key=value assignment: '%s'.\n", args[i])
-			os.Exit(5)
+			os.Exit(errInput)
 		}
 
 		_, ok := replaces[split[0]]
 		if ok {
 			fmt.Printf("Key: '%s' is defined multiple times in arguments.\n", split[0])
-			os.Exit(5)
+			os.Exit(errInput)
 		}
 
 		if split[0] == "" {
 			fmt.Printf("A key can not be an empty string.\n")
-			os.Exit(6)
+			os.Exit(errInput)
 		}
 
 		regKeyMatch, err := regexp.Compile(split[0])
@@ -62,21 +68,17 @@ func main() {
 		if err != nil {
 			fmt.Printf("Key '%s' is not regex compliant. Error: '%s'.\n",
 				split[0], err.Error())
-			os.Exit(7)
+			os.Exit(errInput)
 		}
 
 		matches := regKeyMatch.FindAllString(fileContentStr, -1)
-		for _, v := range matches {
-			fmt.Println(v)
-		}
-
 		if len(matches) == 0 {
 			fmt.Printf("Key: '%s' not found in file.\n", split[0])
-			os.Exit(8)
+			os.Exit(errInput)
 		} else if len(matches) > 1 {
 			fmt.Printf("Key: '%s' is defined %v times in the file: '%s'.\n",
 				split[0], len(matches), args[0])
-			os.Exit(9)
+			os.Exit(errInput)
 		}
 
 		replaces[matches[0]] = split[1]
@@ -93,7 +95,7 @@ func main() {
 	for {
 		if currentTries == 0 {
 			fmt.Printf("Could not create a unique temp file name after %d tries.\n", maxTries)
-			os.Exit(8)
+			os.Exit(errInternal)
 		}
 
 		guid := uuid.New().String()
@@ -112,19 +114,19 @@ func main() {
 	err = ioutil.WriteFile(tempFilename, []byte(fileContentStr), 0644)
 	if err != nil {
 		fmt.Printf("Error writing file at: '%s' error: '%s.\n", args[0], err.Error())
-		os.Exit(9)
+		os.Exit(errInternal)
 	}
 
 	err = os.Remove(args[0])
 	if err != nil {
 		fmt.Printf("Error clearing file at: '%s' error: '%s.\n", args[0], err.Error())
-		os.Exit(10)
+		os.Exit(errInternal)
 	}
 
 	err = os.Rename(tempFilename, args[0])
 	if err != nil {
 		fmt.Printf("Could not restore original file name: '%s.\n", err.Error())
-		os.Exit(11)
+		os.Exit(errInternal)
 	}
 
 	fmt.Println("Success.")
