e03a869c8ee3c6b4b900af81643cc34a4ac292be
Onur YURDUPAK <onuryurdupak@gmail.com>
2022-01-01 23:45:20 +0300

    Add build for Windows and Linux

--- interpolator/build.sh
+++ interpolator/build.sh
@@ -12,5 +12,10 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 interpolator ./main.go ':=' 'stamp_build_date\s+=\s+"\${build_date}":=stamp_build_date = '\"$DATE\"
 interpolator ./main.go ':=' 'stamp_commit_hash\s+=\s+"\${commit_hash}":=stamp_commit_hash = '\"$COMMIT_HASH\"
 
+go env -w GOOS=windows GOARCH=amd64
 go build
+
+go env -w GOOS=linux GOARCH=amd64
+go build
+
 git reset --hard
