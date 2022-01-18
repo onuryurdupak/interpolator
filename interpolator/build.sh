1f771b2eefcc854cea34dcaba993fd957112e27b
Onur YURDUPAK <onuryurdupak@gmail.com>
2022-01-18 21:09:17 +0300

    Refactor build.sh

--- interpolator/build.sh
+++ interpolator/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-git diff --quiet HEAD
 
-if [ "$?" != "0" ]; then
+git diff --quiet HEAD
+code=$?
+
+if [ "$code" != "0" ]; then
     echo "Error: Can not build: repository is dirty."
     exit 1
 fi
@@ -10,14 +12,17 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 DATE=$(date +'%Y.%m.%d')
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-interpolator $REPO_ROOT/interpolator/embed/data.go ':=' 'Stamp_build_date\s+=\s+"\${build_date}":=Stamp_build_date = '\"$DATE\"
+# Get interpolator from https://github.com/onuryurdupak/interpolator
+# shellcheck disable=SC2016
+interpolator "$REPO_ROOT/interpolator/embed/data.go" ':=' 'Stamp_build_date\s+=\s+"\${build_date}":=Stamp_build_date = '\""$DATE"\"
 code=$?
 if [ "$code" != "0" ]; then
     echo "Error: Attempt to run interpolator exited with code: $code."
     exit $code
 fi
 
-interpolator $REPO_ROOT/interpolator/embed/data.go ':=' 'Stamp_commit_hash\s+=\s+"\${commit_hash}":=Stamp_commit_hash = '\"$COMMIT_HASH\"
+# shellcheck disable=SC2016
+interpolator "$REPO_ROOT/interpolator/embed/data.go" ':=' 'Stamp_commit_hash\s+=\s+"\${commit_hash}":=Stamp_commit_hash = '\""$COMMIT_HASH"\"
 code=$?
 if [ "$code" != "0" ]; then
     echo "Error: Attempt to run interpolator exited with code: $code."
